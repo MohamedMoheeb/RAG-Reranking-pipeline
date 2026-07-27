ac5814b9282086a93867d649d10751907ba8b4ab
MohamedMoheeb <60315884+MohamedMoheeb@users.noreply.github.com>
2026-07-27 12:21:28 +0300

    Update __all__ to include ParentChildIndexer and GeminiGenerator

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
-from .indexer import DocumentIndexer
+from .indexer import ParentChildIndexer
 from .retriever import VectorRetriever
 from .reranker import FlashRankReranker
+from .generator import GeminiGenerator
 
-__all__ = ["DocumentIndexer", "VectorRetriever", "FlashRankReranker"]
+__all__ = ["ParentChildIndexer", "VectorRetriever", "FlashRankReranker", "GeminiGenerator"]
